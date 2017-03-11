084fd8724124be89583c551d50e80c7a0f7205b8
amsjacks <aislin.ms.jackson@gmail.com>
2017-03-11 11:15:05 -0800

    Director class skeleton created

---
+++ src/models/Director.java
@@ -0,0 +1,12 @@
+package models;
+
+import java.util.Set;
+
+/**
+ * Created by aislin on 2017-03-11.
+ */
+public class Director {
+        private Set<Identity> identitySet;
+        private Set<Movie> moviesDirected;
+
+}
